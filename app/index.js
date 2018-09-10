c0a35ecb52acbe60583dcc012d7a47ed2bf067c2
Vitor George <vitor.george@gmail.com>
2018-09-10 12:04:21 +0100

    Reorganize directories

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var villagesRouter = require('./routes/villages');
+var dailyViirsRouter = require('./routes/daily-viirs');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/villages', villagesRouter);
+app.use('/daily-viirs', dailyViirsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
