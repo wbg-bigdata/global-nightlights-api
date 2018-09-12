056be37165c0885e7c34b3e43083cf146c469f3f
Vitor George <vitor.george@gmail.com>
2018-09-12 15:56:49 +0100

    Remove unused models and services

--- app/services/index.js
+++ app/services/index.js
@@ -1,7 +1,5 @@
-const positions = require("./positions/positions.service.js");
 const observations = require("./observations.js");
 
 module.exports = function(app) {
-  app.configure(positions);
   app.configure(observations);
 };
