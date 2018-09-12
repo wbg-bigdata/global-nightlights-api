394504214496b206595c14a1bc8a720a5696b3ef
Vitor George <vitor.george@gmail.com>
2018-09-12 15:47:56 +0100

    Remove ESLint rule

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
+const logger = require("./logger");
+const app = require("./app");
+const port = app.get("port");
 const server = app.listen(port);
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
+process.on("unhandledRejection", (reason, p) =>
+  logger.error("Unhandled Rejection at: Promise ", p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Global Night Lights Data Service started on http://%s:%d', app.get('host'), port)
+server.on("listening", () =>
+  logger.info(
+    "Global Night Lights Data Service started on http://%s:%d",
+    app.get("host"),
+    port
+  )
 );
